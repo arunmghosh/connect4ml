255b209d5d3bd512777662b1a648ebade758d186
Arun Ghosh <aghosh@mail.wlu.edu>
2026-06-15 15:30:34 -0400

    started functionality for encoding positions

--- games/ConnectFour.py
+++ games/ConnectFour.py
@@ -6,41 +6,64 @@ class ConnectFour(DeterministicGame):
 
     empty_board = np.empty([5, 6], dtype=str)
 
-    def __init__(self, players, start_player):
+    def __init__(self, players):
         # players is an array of 2 Player objects
         # start_player is the index of the starting player
         # board_state is a 2d array (6 x 7 grid) of strings ("", "R", "Y")
-        super().__init__("Connect 4", players, start_player, ConnectFour.empty_board)
+        super().__init__("Connect 4", players, ConnectFour.empty_board, "connect_4_transposition.pkl")
 
         # useful information about the board
         self.player_tokens = np.array(["R", "Y"])
-        self.col_slots_left = np.array([6, 6, 6, 6, 6, 6, 6], dtype=int)
+        self.col_heights = np.array([0, 0, 0, 0, 0, 0, 0], dtype=int)
+
+        # search board
+        self.pointer = ""  # stores color of streak being analyzed, will store winner if 4-streak found
+        self.current_streak = 0
+        self.traversal_steps = 0  # tracks how much of a row, column or diagonal has been checked
 
     def update_grid(self, move):
         # player is the index of the current player
         # move is the column index in which the player places a token
 
         # find the lowest empty slot in column
-        next_slot = 6 - self.col_slots_left[move]
+        next_slot = self.col_slots_left[move]
 
         # update board state
         self.board_state[next_slot][move] = self.player_tokens[self.current_player]
-        self.col_slots_left[move] -= 1
+        self.col_heights[move] += 1
 
     def check_for_four(self, row_inc, col_inc, row, col):
-        # base_units is the number of rows, columns or diagonals
-        # row_dir and col_dir are values of incrementation of the pointers row and col (-1, 0, 1)
-        # row and col are the coordinates of the starting point in the grid (between (0,0) and (5,6))
-        start_color = self.board_state[row][col]
+        # (row, col) is starting coordinate
+        # row_inc and col_inc tell us direction of search
+
+        self.current_streak = 0
+        self.traversal_steps = 0
+        self.pointer = self.board_state[row][col]  # stores color of current streak
         row_pointer = row
         col_pointer = col
-        if start_color != "":
-            for i in range(3):
-                row_pointer += row_inc
-                col_pointer += col_inc
-                if self.board_state[row_pointer][col_pointer] != start_color:
-                    return False
-        return True
+
+        while self.traversal_steps < 7:
+            if row_pointer > 5 or col_pointer > 6:
+                return False  # passed the edge of the grid, search complete
+            if self.pointer != "":
+                if self.board_state[row_pointer][col_pointer] != self.pointer:
+                    if self.traversal_steps > 3:
+                        return False  # not enough spaces left to get 4 in a row
+                    self.pointer = self.board_state[row_pointer][col_pointer]  # switch colors case
+                else:
+                    self.current_streak += 1
+                    if self.current_streak == 4:
+                        return True  # token of winner stored in self.pointer
+            else:
+                if self.traversal_steps > 2:
+                    return False  # not enough spaces left to get 4 in a row
+                self.current_streak = 0
+                self.pointer = self.board_state[row_pointer + row_inc][col_pointer + col_inc]
+            row_pointer += row_inc
+            col_pointer += col_inc
+            self.traversal_steps += 1
+        self.pointer = ""  # if the game is over and no 4-streak is found, it's a draw
+        return False
 
     def get_possible_moves(self):
         # first check for a four in a row, and return an empty list if found
@@ -60,7 +83,7 @@ class ConnectFour(DeterministicGame):
                     return []
 
         # now check bottom right to top left diagonal
-        upper_bound = 6 - self.col_slots_left[6]
+        upper_bound = self.col_heights[6]
         for row in range(0, upper_bound):
             if self.check_for_four(1, -1, row, 6):
                 return []
@@ -69,7 +92,7 @@ class ConnectFour(DeterministicGame):
                 return []
 
         # now check bottom left to top right diagonal
-        upper_bound = 6 - self.col_slots_left[0]
+        upper_bound = self.col_heights[0]
         for row in range(0, upper_bound):
             if self.check_for_four(1, 1, row, 0):
                 return []
@@ -80,6 +103,14 @@ class ConnectFour(DeterministicGame):
         # now check if there are any columns with open slots
         open_slots = []
         for col in range(7):
-            if self.col_slots_left[col] > 0:
+            if self.col_heights[col] < 6:
                 open_slots.append(col)
         return open_slots
+
+    def determine_outcome(self):
+        if self.pointer == "R":
+            self.update_result(self.players[0] + " wins!")
+        elif self.pointer == "Y":
+            self.update_result(self.players[1] + " wins!")
+        else:
+            self.update_result("Draw.")
--- games/DeterministicGame.py
+++ games/DeterministicGame.py
@@ -1,16 +1,34 @@
+import pickle
+
+
 class DeterministicGame:
-    def __init__(self, name, players, start_player, board_state):
+    def __init__(self, name, players, board_state, filename):
         self.name = name
+
+        # player related attributes
         self.players = players  # array of Player objects
         self.num_players = len(players)
-        self.current_player = start_player  # player moving, index
+        self.current_player = 0  # player moving, index
+
+        # game status attributes
         self.game_finished = False  # finished = True
         self.board_state = board_state  # where moves are in the array
+        self.result = "message overridden in child class"
 
+        # storing game data
+        self.filename = filename
+        try:
+            with open(self.filename, "rb") as f:
+                self.recorded_positions = pickle.load(f)
+        except FileNotFoundError:
+            self.recorded_positions = {}  # table to store game data in
+
+    # player related methods
     def switch_turn(self):
         self.current_player += 1
         self.current_player = self.current_player % self.num_players
 
+    # game status related methods
     def get_possible_moves(self):
         return self.board_state  # override in child classes
 
@@ -21,9 +39,24 @@ class DeterministicGame:
         if not self.get_possible_moves():
             self.game_finished = True
 
+    def update_result(self, msg: str):
+        self.result = msg
+
+    # data collection related methods
+    def encode(self):
+        return self.board_state  # override in child classes
+        # new positions will be added to the transposition table
+
+    def export_new_data(self):
+        with open(self.filename, "wb") as f:
+            pickle.dump(self.recorded_positions, f)
+
+    # gameplay mechanic
     def play_game(self):
         while not self.game_finished:
-            move = self.current_player.move(self.get_possible_moves())
+            move = self.players[self.current_player].move(self.get_possible_moves())
             self.update_grid(move)
             self.update_status()
             self.switch_turn()
+        print(self.result)
+        self.export_new_data()
